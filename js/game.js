dad160e1c781069512dbc52f0cf704e90ea933c9
Nicholas Kebbas <nicholas.kebbas@yahoo.com>
2015-07-06 00:12:38 -0700

    Update game.js

--- js/game.js
+++ js/game.js
@@ -51,7 +51,8 @@ addEventListener("keyup", function (e) {
 
 // Reset the game when the player catches a monster
 var reset = function () {
-
+	hero.x = canvas.width / 2;
+	hero.y = canvas.height / 2;
 
 	// Throw the monster somewhere on the screen randomly
 	monster.x = 140 + (Math.random() * (canvas.width - 200));
