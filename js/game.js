963ee1af343cc1b29a3f8d2ad1639d739d541cd0
Nicholas Kebbas <nicholas.kebbas@yahoo.com>
2015-07-05 23:59:05 -0700

    Update game.js

--- js/game.js
+++ js/game.js
@@ -1,8 +1,8 @@
 // Create the canvas
 var canvas = document.createElement("canvas");
 var ctx = canvas.getContext("2d");
-canvas.width = 1024;
-canvas.height = 960;
+canvas.width = 800;
+canvas.height = 600;
 document.body.appendChild(canvas);
 
 // Background image
